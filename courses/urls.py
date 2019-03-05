b2c0ff842a41eac6c678c571976405b91a91ea9f
Kennedy Njeri <kennedy@Kennedys-MacBook-Air.local>
2019-03-05 13:18:29 +0300

    Initial commit. Added serializers of the models

---
+++ courses/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.conf.urls import include
+
+
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+
+urlpatterns = [
+
+    #url(r'^hello-view/', views..as_view()),
+    #url(r'', include(router.urls))
+
+]

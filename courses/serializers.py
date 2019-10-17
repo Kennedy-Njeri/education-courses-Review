f7204d70d22d204b8a275a09e5d31d6c8dc3bac8
Kennedy Njeri <mistakenz123@gmail.com>
2019-10-17 23:52:30 +0300

    customizeserializers

--- courses/serializers.py
+++ courses/serializers.py
@@ -21,7 +21,11 @@ class ReviewSerializer(serializers.ModelSerializer):
         )
         model = models.Review
 
+    def validate_rating(self, value):
+        if value in range(1, 6):
+            return value
 
+        raise serializers.ValidationError('Rating must be an integer between 1 and 5')
 
 
 class CourseSerializer(serializers.ModelSerializer):
